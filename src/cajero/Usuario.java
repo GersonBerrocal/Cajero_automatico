23240507a1a03d47746a5c4a775c3486f4cca2e3
Gerson berrocal <gwrsonberrocal@gmail.com>
2021-03-25 13:24:37 -0500

    Se modifico la clase Usuario Añadio setters y getters metodos de retirar transferir modificar

--- src/cajero/Usuario.java
+++ src/cajero/Usuario.java
@@ -1,20 +1,63 @@
 package cajero;
 
 public class Usuario {
-	String nombre;
-	int num_tarjeta;
-	float saldo;
-	String contrasena;
-	public Usuario(int cod,String contrasena, float saldo, String nom) {
-		this.nombre=nom;
-		this.num_tarjeta=cod;
-		this.saldo=saldo;
-		this.contrasena=contrasena;
-	}
-	public float ver_saldo() {
+	private String nombre;
+	private int tarjeta;
+	private float saldo;
+	private String contrasena;
+
+	public float getSaldo() {
 		return this.saldo;
 	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getTarjeta() {
+		return this.tarjeta;
+	}
+
+	public void setTarjeta(int tarjt) {
+		this.tarjeta = tarjt;
+	}
+
+	public String getContrasena() {
+		return this.contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	// Constructor
+	public Usuario(int numero, String contrasena, float saldo, String nombre) {
+		setTarjeta(numero);
+		setContrasena(contrasena);
+		setSaldo(saldo);
+		setNombre(nombre);
+
+	}
+
+	// Metodos de usuario
+
 	public void retirar(float cantidad_retirada) {
-		this.saldo=this.saldo-cantidad_retirada;
+		this.saldo = this.saldo - cantidad_retirada;
+	}
+
+	public void transferir(int cuenta, float cantidad) {
+		this.saldo -= cantidad;
+	}
+
+	public void depositar(float cantidad) {
+		this.saldo += cantidad;
 	}
 }
